f3f50807fff195857018e232d4f0e732a52bc579
abrahax01 <56842663+abrahax01@users.noreply.github.com>
2021-02-15 10:06:10 +0000

    Create ModulusOperator.c

---
+++ Semester1/Labs/Lab2/ModulusOperator.c
@@ -0,0 +1,34 @@
+/*      Program Description: this program calculates the Modulus Operator
+        Author: Abraham Izarra
+        Date: 30 sept 2019
+*/
+
+#include <stdio.h>
+int main()
+
+{
+    
+    int ans = 0;
+    int ans2 = 0;
+    int ans3 = 0;
+    int ans4 = 0;
+    int ans5 = 0;
+    int ans6 = 0;
+    
+    ans = 2 % 2;
+    ans2 = 3 % 2;
+    ans3 = 5 % 2;
+    ans4 = 7 % 3;
+    ans5 = 100 % 33;
+    ans6 = 100 % 7;
+    
+    printf ("ans is %d \n", ans);
+    printf ("ans2 is %d \n", ans2);
+    printf ("ans3 is %d \n", ans3);
+    printf ("ans4 is %d \n", ans4);
+    printf ("ans5 is %d \n", ans5);
+    printf ("ans6 is %d \n", ans6);
+    
+    return 0;
+    
+}
