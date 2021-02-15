263683a8a8abee9d54204fc4d94aa96f2df94b9e
abrahax01 <56842663+abrahax01@users.noreply.github.com>
2021-02-15 10:02:13 +0000

    Create Address.c

---
+++ Semester1/Labs/Lab1/Address.c
@@ -0,0 +1,16 @@
+/*      Description: this code is to output my name and address in two different lines
+        Author: Abraham Izarra
+        Date: 23 sept 2019
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Abraham Izarra \n");
+    printf("14 Church View \nBallymahon \nCo.Longford \n");
+    printf("Date: 23 sept 2019 \n");
+    
+    return 0;
+ }
+
